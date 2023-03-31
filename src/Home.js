a8eed71994d259c78e47b87ed734b64aefc2aae2
shihuai <tanshihuai@gmail.com>
2023-04-01 04:09:29 +0800

    useEffects

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import BlogList from './BlogList'
 
 const Home = () => {
@@ -14,6 +14,10 @@ const Home = () => {
         const newBlogs = blogs.filter(blog => blog.id !== id );
         setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        console.log("hello world")
+    }, [])
 
     return (
         <div className = "home">
