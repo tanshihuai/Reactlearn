b2e2256af6bcffe0437f894f8e1b939d3c617b4f
shihuai <tanshihuai@gmail.com>
2023-04-08 23:55:30 +0800

    26

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,22 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
     const { id } = useParams()
+    const { data: blog, isLoading, hasError } = useFetch(`http://localhost:8000/blogs/${id}`)
 
     return ( 
         <div className="blog-details">
-            <h2>Blog details - {id}</h2>
+            { isLoading && <div>Loading...</div> }
+            { hasError && <div>{ hasError }</div> }
+            { blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
      );
 }
